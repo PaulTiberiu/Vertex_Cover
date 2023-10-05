5105dd4bc6161cfabbac80209b662570bac7568e
Iordache Paul-Tiberiu <123265734+PaulTiberiu@users.noreply.github.com>
2023-10-05 19:42:03 +0200

    Update Graph.py

--- Graph.py
+++ Graph.py
@@ -4,34 +4,32 @@ import numpy as np
 # V = ca doit etre un tableau
 
 class Graph:
-    V = set     # Ensemble de sommets (la liste de sommets) 
-    E = dict    # Dictionnaire de aretes (la liste d'arretes)
+    V = set # Ensemble de sommets (la liste de sommets) 
+    E = dict # Dictionnaire de aretes (la liste d'arretes)
 
 
     def __init__(self, V, E=None):
         self.V = V
 
         if(E == None):
-            self.E = {i : set() for i in V}     # Creer le dictionnaire qui relie le sommet i a vide
+            self.E = {i : set() for i in V} # Creer le dictionnaire qui relie le sommet i a vide
         else:
             self.E = E
 
-    def insert_edge(self, v1, v2):
-        """Permet d'ajouter une arrete"""
+    def insert_edge(self, v1, v2): # Permet d'ajouter une arrete
         self.E[v1].add(v2)          # On ajoute v1 a v2 et
         self.E[v2].add(v1)          # v2 a v1 car le graphe est non oriente
 
 #Question 2.1.1
 
-    def delete_vertex(self, v):
-        """Retourne un nouveau graphe G_cpy sans le sommet v"""
+    def remove_vertex(self, v):     # Retourne un nouveau graphe G_cpy sans le sommet v
 
         G_cpy = copy.deepcopy(self)   # On cree une copie independante de notre Graphe pour pouvoir renvoyer un nouveau Graphe G' 
 
         if v not in G_cpy.V :         # S'il n'est pas dans l'ensemble de sommets, on quitte de la fonction
             return
         
-        for vertex in G_cpy.E[v]:     # Apres avoir supprimme toutes les areetes du sommet v
+        for vertex in G_cpy.E[v]:    # Apres avoir supprimme toutes les areetes du sommet v
             G_cpy.E[vertex].remove(v)
 
         del G_cpy.E[v]               # On supprime le sommet dans le dictionnaire d'arretes
@@ -42,11 +40,10 @@ class Graph:
 
 #Question 2.1.2
     
-    def delete_many_vertex(self, set_delete) : 
-        """ Retourne un nouveau graphe G_cpy sans les sommets de l'ensemble set_delete"""
-        G_cpy = copy.deepcopy(self)     # On cree une copie independante de notre Graphe pour pouvoir renvoyer un nouveau Graphe G' 
+    def remove_many_vertex(self, set_delete) : # Retourne un nouveau graphe G_cpy sans les sommets de l'ensemble set_delete
+        G_cpy = copy.deepcopy(self)   # On cree une copie independante de notre Graphe pour pouvoir renvoyer un nouveau Graphe G' 
 
-        for i in set_delete:            # On reeutilise l'algorithme de la fonction remove_vertex en faisant une boucle sur les valeurs de set_delete
+        for i in set_delete: # On reeutilise l'algorithme de la fonction remove_vertex en faisant une boucle sur les valeurs de set_delete
 
             if i not in G_cpy.V :         
                 return                 
@@ -59,16 +56,22 @@ class Graph:
             
         return G_cpy
 
-#Question 2.1.3
+    #Question 2.1.3
 
-    def nombre_degre(self) :        
+    """
+    def ens_to_tab(Ens):
+        T = np.array(list(Ens))  # Convertissez l'ensemble dans une liste, puis en un tableau numpy
+        return T
+    """
+
+    def degree_number(self) :        
         """Renvoie un nouveau dictionnaire qui associe chaque sommet (clé) à son degré (valeur)"""
         deg = dict()
         for i in self.E :
             deg[i] = len(self.E[i]) 
         return deg
 
-    def max_degre(self) :           
+    def max_degree(self) :           
         """Cherche le degré maximum dans le dictionnaire de { sommet : degré }"""
         dico_deg = self.nombre_degre()
         candMax = -1
@@ -76,5 +79,3 @@ class Graph:
             if dico_deg[e] > candMax :
                 candMax = dico_deg[e]
         return candMax
-
-
